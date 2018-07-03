78a9b32644577a890e310dacf0d330db821b8e76
masila <abelmasila@gmail.com>
2018-07-03 18:53:45 +0300

    Added jwt token auth

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 const PORT = process.env.PORT || 8888;
+//add body parser middleware
 app.use(bodyParser.json());
+//users database
+const users = [
+  { id: 1, username: 'Admin', password: 'admin' },
+  { id: 2, username: 'abel', password: 'abel' },
+  { id: 3, username: 'masila', password: 'masila' }
+];
 
 app.get('/status', (req, res) => {
   const localTime = new Date().toLocaleDateString();
   res.status(200).send(`Server time is ${localTime}`);
 });
 app.post('/login', (req, res) => {
-  const user = req.body.username;
-  res.status(200).send(`You logged in with ${user}`);
+  if (!req.body.username || !req.body.password) {
+    res.status(400).send('You need a username and password');
+    return;
+  }
+  const user = users.find(u => {
+    return u.username === req.body.username && u.password === req.body.password;
+  });
+  if (!user) {
+    res.status(401).send('User not found!');
+    return;
+  }
+  //if valid user, return json web token
+  const token = jwt.sign(
+    {
+      sub: user.id,
+      username: user.username
+    },
+    'secret',
+    { expiresIn: '3 hours' }
+  );
+
+  res.status(200).send({ access_token: token });
 });
 app.get('*', (req, res) => {
   res.sendStatus(404);
