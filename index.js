5d505ef9ba8bee92df251e1d6d1376ca13629f35
masila <abelmasila@gmail.com>
2018-07-03 18:24:57 +0300

    Added test login route

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = 8888;
+const PORT = process.env.PORT || 8888;
+app.use(bodyParser.json());
 
 app.get('/status', (req, res) => {
   const localTime = new Date().toLocaleDateString();
   res.status(200).send(`Server time is ${localTime}`);
 });
-
+app.post('/login', (req, res) => {
+  const user = req.body.username;
+  res.status(200).send(`You logged in with ${user}`);
+});
 app.get('*', (req, res) => {
   res.sendStatus(404);
 });
